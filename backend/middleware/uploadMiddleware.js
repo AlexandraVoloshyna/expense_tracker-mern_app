2d1c4b1fe8ab65f292c80f5d76defb4ab8c3c401
AlexandraVoloshyna <125306852+AlexandraVoloshyna@users.noreply.github.com>
2023-08-09 12:44:14 +0300

    message

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,7 +1,7 @@
 import multer from "multer";
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'backend/images');
+      cb(null, 'https://expense-tracker-server-rp9x.onrender.com');
     },
     filename: (req, file, cb) => {
       cb(null, Date.now() + '-' + file.originalname);
